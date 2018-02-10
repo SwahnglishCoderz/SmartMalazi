d5360b0a78303ef832a3971b6c4fa683a5ac9455
Joel Kibona <joelvankibona@gmail.com>
2018-02-10 16:25:34 +0300

    Added email support for online web

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -57,7 +57,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'smartmalazi@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'smartMalazi'),
+        'name' => env('MAIL_FROM_NAME', 'SmartMalazi-Admin'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('MAIL_USERNAME','smartmalazi@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('MAIL_PASSWORD','smartjosa4343'),
 
     /*
     |--------------------------------------------------------------------------
